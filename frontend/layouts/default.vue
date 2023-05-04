635e8615049a130c424d1b8d12b4e714c2ecabf2
Takeshi Kimata <117462761+kimatata@users.noreply.github.com>
2023-05-04 18:55:40 +0900

    URL入力画面作成

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <div>Header Here</div>
+    <div>
+      <nav class="navbar shadow">
+        <div class="container-fluid">
+          <NuxtLink :to="'/'" class="navbar-brand" >
+            <img src="/autoread.png" alt="AutoRead" width="30"/>
+            <span class="ms-2">AutoRead</span>
+          </NuxtLink>
+        </div>
+      </nav>
+    </div>
     <slot />
   </div>
 </template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        Sidebar
+  <div class="container-fluid">
+
+    <!-- url input -->
+    <div class="row mt-5">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <label class="form-text" for="siteUrlInput">type any website's url you want to read as video.</label>
+        <form>
+          <div class="input-group">
+            <input type="url" class="form-control" id="siteUrlInput">
+            <button type="submit" class="btn btn-primary">start</button>
+          </div>
+        </form>
       </div>
-      <div class="col-md-9">
-        <div class="btn btn-primary">search web site you want to audible</div>
+      <div class="col-md-3"></div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-9 bg-light">
+        video pane
+      </div>
+      <div class="col-md-3 bg-light">
+        Play list..
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+
 </script>
 
 <style scoped></style>
